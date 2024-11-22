8c121ee5073bc72e5a571c1dfac8f71929b60f9a
simon <simon.dufac@gmail.com>
2024-11-22 16:36:17 +0100

    Bien setup le serveur node pour que monscript d'avant fonctionne

--- ts/server.ts
+++ ts/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import path from 'path';
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '../dist')))
+
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(port, () => {
 	return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+
