9b89953cb93db074c277b11c84a40a947e35b63c
simon <simon.dufac@gmail.com>
2024-12-11 16:52:13 +0100

    Transcription en français

--- ts/deepspeechprocess.ts
+++ ts/deepspeechprocess.ts
@@ -7,8 +7,11 @@ export function speechToText(audioPath: string): string {
 	const MODEL_PATH = path.join(__dirname, "dsModel", "deepspeech-0.9.3-models.pbmm");
 	const SCORER_PATH = path.join(__dirname, "dsModel", "deepspeech-0.9.3-models.scorer");
 
-	const model = new DeepSpeech.Model(MODEL_PATH);
-	model.enableExternalScorer(SCORER_PATH);
+	const FRENCH_MODEL_PATH = path.join(__dirname, "dsModel", "frenchTS", "output_graph.pbmm");
+	const FRENCH_SCORER_PATH = path.join(__dirname, "dsModel", "frenchTS", "kenlm.scorer");
+
+	const model = new DeepSpeech.Model(FRENCH_MODEL_PATH);
+	model.enableExternalScorer(FRENCH_SCORER_PATH);
 
 	const audioData = fs.readFileSync(audioPath);
 
